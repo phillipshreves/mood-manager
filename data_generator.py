635c09d5e2e3fa76e9e35afcb63ec3e84cbe5661
Phillip Shreves <hi@phillips.work>
2020-03-31 20:32:42 -0500

    Pandas DataFrame matched with Plotly graph

--- data_generator.py
+++ data_generator.py
@@ -9,25 +9,24 @@ path_data = path_root / 'data' / '2020-03-27'
 
 
 # Get Data
-entry_data_frame = pandas.read_csv(path_data / 'entry.csv', index_col=1)
+entry_data_frame = pandas.read_csv(path_data / 'entry.csv', index_col=0)
 # The date column comes in with an silly name, so just rename to DATE
-entry_data_frame.index.rename('DATE', inplace=True)
-#entry_data_frame.set_index('DATE', inplace = True)
-print(entry_data_frame)
+entry_data_frame.rename(columns = {'DATE (YYYY-MM-DD)':'DATE'}, inplace=True)
 
 
 # Drop irrelevant columns
-entry_data_frame.drop(['ID', 'PSYCHOTIC SYMPTOMS', 'NOTE','THERAPY','WEIGHT','MENSTRUAL CYCLE'], inplace = True, axis=1)
-print(entry_data_frame)
+entry_data_frame.drop(['PSYCHOTIC SYMPTOMS', 'NOTE','THERAPY','WEIGHT','MENSTRUAL CYCLE'], inplace = True, axis=1)
 
+entry_data_frame.set_index('DATE', inplace=True)
+
+# Add multilevel for stack
+levels_to_add = [('CATEGORIES','IRRITABILITY'),('CATEGORIES','ANXIETY'),('CATEGORIES','DEPRESSED'),('CATEGORIES','ELEVATED'),('CATEGORIES','SLEEP')]
+entry_data_frame.columns = pandas.MultiIndex.from_tuples(levels_to_add)
 
 # Create records from the columns so that we can plot the data from each column in one plot
-entry_data_frame_stacked = (entry_data_frame.stack().reset_index(name='VALUES').set_index('DATE').rename(columns={'level_1':'CATEGORIES'}))
+entry_data_frame_stacked = entry_data_frame.stack().reset_index().rename(columns={'level_1':'CATEGORIES','CATEGORIES':'VALUES'}).set_index('DATE')
 print(entry_data_frame_stacked)
 
-
 # Create the graph
-#fig = plotly.express.bar(entry_data_frame, y='DATE')
-#fig = plotly.express.scatter(entry_data_frame, x='CATEGORIES', y='DATE')
-#fig = plotly.express.scatter_matrix(data_entry)
-#fig.show()
+fig = plotly.express.scatter(entry_data_frame_stacked, x='CATEGORIES', y=entry_data_frame_stacked.index, size='VALUES')
+fig.show()
