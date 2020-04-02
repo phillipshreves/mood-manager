81172cc667544805ba49e60f063aba9b24b754a2
Phillip Shreves <hi@phillips.work>
2020-04-01 21:19:19 -0500

    Ready to create Dash web app

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,9 @@
 import plotly.express
 import pandas
+import dash
+import dash_core_components
+import dash_html_components
+from dash.dependencies import Inpurt, Output
 from pathlib import Path
 
 
@@ -23,13 +27,36 @@ entry_data_frame.set_index('DATE', inplace=True)
 levels_to_add = [('CATEGORIES','IRRITABILITY'),('CATEGORIES','ANXIETY'),('CATEGORIES','DEPRESSED'),('CATEGORIES','ELEVATED'),('CATEGORIES','SLEEP')]
 entry_data_frame.columns = pandas.MultiIndex.from_tuples(levels_to_add)
 
+# The data uses 1 for values when the value is "None", so set 1 to 0, then reduce the size of SLEEP to a max of 4 to keep graph consistent
+def size_index(row):
+    if row['VALUES'] == 1:
+        return 0
+    if row['VALUES'] > 4:
+        return 4
+    return row['VALUES']
+
+
 # Create records from the columns so that we can plot the data from each column in one plot
 entry_data_frame_stacked = entry_data_frame.stack().reset_index().rename(columns={'level_1':'CATEGORIES','CATEGORIES':'VALUES'}).set_index('DATE')
-print(entry_data_frame_stacked)
+
+# Add size index
+entry_data_frame_stacked['SIZE'] = entry_data_frame_stacked.apply(
+    lambda row: size_index(row), axis = 1
+)
 
 # Create the graph
 height_records = len(entry_data_frame_stacked.index) * 7
-fig = plotly.express.scatter(entry_data_frame_stacked, x='CATEGORIES', y=entry_data_frame_stacked.index, size='VALUES', hover_data=['VALUES'], height=height_records)
+fig = plotly.express.scatter(
+    entry_data_frame_stacked, 
+    x='CATEGORIES',
+    y=entry_data_frame_stacked.index, 
+    size='SIZE', 
+    hover_data=['VALUES'], 
+    height=height_records, 
+    color='SIZE' 
+)
+
+# Style graph
 fig.update_layout(
     xaxis=dict(
         side='top'
@@ -37,6 +64,30 @@ fig.update_layout(
     yaxis=dict(
         tickmode='linear',
         type='date'
+    ),
+    coloraxis_colorbar=dict(
+        thicknessmode='pixels',
+        thickness=40,
+        lenmode='pixels', 
+        len=300,
+        yanchor='top',
+        y=1,
+        title='Intensity',
+        dtick=1
     )
 )
+
+fig.update_traces(
+    marker=dict(
+        line=dict(
+            width=1,
+            color='DarkSlateGrey'
+        )
+    ),
+    selector=dict(
+        mode='markers'
+    )
+)
+
+# Display
 fig.show()
